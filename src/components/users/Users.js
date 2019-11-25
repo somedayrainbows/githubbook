dc271690f5e47eb92896468e39130c064c84aedf
Erin Bassity <ebassity@shutterstock.com>
2019-11-24 21:02:42 -0700

    Convert class to functional components
    * Add axios
    * Add spinner component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,40 +1,31 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'http://github.com/mojombo'
-      },
-      {
-        id: 2,
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'http://github.com/defunkt'
-      },
-      {
-        id: 3,
-        login: 'pjhyett',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-        html_url: 'http://github.com/pjhyett'
-      }
-    ]
-  }
-  render() {
-    const { users } = this.state
-    
+const Users = ({ users, loading }) => {
+  if(loading) {
+    return <Spinner />
+  } else {
     return (
-      <div>
+      <div style={userStyle}>
         {users.map(user => {
           return <UserItem key={user.id} user={user} />
         })}
       </div>
-    )
-    }
+    )  
+  }
+}
+
+Users.protTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
+}
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
 }
 
 export default Users
