3479d276e34d3ae0c004a8bad7c7e0e43c8ee6b9
Erin Bassity <ebassity@shutterstock.com>
2019-12-16 20:59:00 -0700

    Add react-router-dom & about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+      <h1>About GithubBook</h1>
+      <p>Welcome! You can use this application to search Github users. It consumes the Github API.</p>
+      <p>Version 1.0.0</p>
+    </>
+  )
+}
+
+export default About
