aaeec5063a1a2012c917275aa92c5c373a40b3d1
Erin Bassity <ebassity@shutterstock.com>
2019-12-17 21:32:03 -0700

    Add repos to user profiles

--- src/App.js
+++ src/App.js
@@ -13,8 +13,9 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
-    alert: null
+    alert: null,
   }
 
   searchUsers = async text => {
@@ -35,6 +36,15 @@ class App extends Component {
     this.setState({ user: res.data, loading: false })
   }
 
+  getUserRepos = async (username) => {
+    this.setState({ loading: true })
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+    
+    this.setState({ repos: res.data, loading: false })
+  }
+
   clearUsers = () => this.setState({ users: [], loading: false })
 
   setAlert = (message, type) => {
@@ -43,7 +53,7 @@ class App extends Component {
   } 
   
   render() {
-    const { users, loading, user } = this.state
+    const { users, loading, user, repos } = this.state
     
     return (
       <Router>
@@ -60,7 +70,7 @@ class App extends Component {
                   showClear={users.length > 0 ? true : false }
                   setAlert={this.setAlert}
                 />
-                <Users loading={loading} users={users}/>
+                <Users loading={loading} users={users} />
               </>
             )} />
             <Route exact path='/about' component={About} />
@@ -68,7 +78,9 @@ class App extends Component {
               <User 
                 { ...props } 
                 getUser={this.getUser} 
+                getUserRepos={this.getUserRepos}
                 user={user} 
+                repos={repos}
                 loading={loading} 
               />
             )} />
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import Spinner from '../layout/Spinner'
+import Repos from '../repos/Repos'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login)
+    this.props.getUserRepos(this.props.match.params.login)
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
   }
 
   render() {
@@ -32,7 +36,7 @@ export class User extends Component {
       hireable 
     } = this.props.user
 
-    const { loading } = this.props
+    const { loading, repos } = this.props
 
     if (loading) return <Spinner  />
 
@@ -52,17 +56,26 @@ export class User extends Component {
             <a href={html_url} className="btn btn-dark my-1">Visit Github Profile</a>
             <ul>
               <li>
-                {login && <><strong>Username: </strong> {login}</>}
+                {login && <>Username: {login}</>}
               </li>
               <li>
-                {company && <><strong>Username: </strong> {company}</>}
+                {company && <>Username: {company}</>}
               </li>
               <li>
-                {blog && <><strong>Username: </strong> {blog}</>}
+                {blog && <>Username: {blog}</>}
               </li>
             </ul>
           </div>  
         </div>
+
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-light'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Gists: {public_gists}</div>
+        </div>
+
+        <Repos repos={repos} />
       </>
     )
   }
