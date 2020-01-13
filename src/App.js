1abc039b9e275f60f4d0902dd586a683e989d7a0
Erin Bassity <ebassity@gmail.com>
2020-01-12 20:44:24 -0700

    Refactor alert and home

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Navbar from './components/layout/Navbar'
-import Users from './components/users/Users'
-import User from './components/users/User'
-import Search from './components/users/Search'
-import Alert from './components/layout/Alert'
-import About from './components/pages/About'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import User from './components/users/User';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
 
-import GithubState from './context/github/GithubState'
-import AlertState from './context/alert/AlertState'
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 import './App.css';
 
-const App = () => {    
-  return (
-    <GithubState>
-      <AlertState>
-        <Router>
-        <div className="App">
-          <Navbar title="GithubBook"/>
-          <div className="container">
-            <Alert />
-            <Switch>
-              <Route exact path='/' render={props => (
-                <>
-                  <Search />
-                  <Users />
-                </>
-              )} />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/user/:login' component={User} />
-            </Switch>
-          </div>
-        </div>
-        </Router>
-      </AlertState>
-    </GithubState>
-  );
-}
+const App = () => {
+	return (
+		<GithubState>
+			<AlertState>
+				<Router>
+					<div className='App'>
+						<Navbar title='GithubBook' />
+						<div className='container'>
+							<Alert />
+							<Switch>
+								<Route exact path='/' component={Home} />
+								<Route exact path='/about' component={About} />
+								<Route exact path='/user/:login' component={User} />
+							</Switch>
+						</div>
+					</div>
+				</Router>
+			</AlertState>
+		</GithubState>
+	);
+};
 
 export default App;
